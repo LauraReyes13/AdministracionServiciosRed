2b939e94c32761995daa54c7813514224e2f6adf
Laura Reyes <89427297+LauraReyes13@users.noreply.github.com>
2022-03-06 22:44:19 -0600

    Update updateSeg.py

--- Practicas/Practica1/updateSeg.py
+++ Practicas/Practica1/updateSeg.py
@@ -15,7 +15,7 @@ for x in range(300):
     valor = "N:" + str(total_input_traffic) + ':' + str(total_output_traffic)
     print (valor)
     rrdtool.update('segmentos.rrd', valor)
-    rrdtool.dump('segmentos.rrd','traficoRED.xml')
+    rrdtool.dump('segmentos.rrd','segmentos.xml')
     time.sleep(1)
 
 if ret:
